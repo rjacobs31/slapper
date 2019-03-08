ff7b7546f8b1529cba4be04deecc2ea0416d01e7
R. Jacobs <jacobsrudolph@yahoo.com>
2019-03-08 16:50:16 +0200

    Add error handling to parse module.

--- src/parse.rs
+++ src/parse.rs
@@ -12,18 +12,18 @@ pub struct SubstitutingUrl {
 }
 
 impl SubstitutingUrl {
-    pub fn from_str(input: &str) -> Self {
-        let mut result = SubstitutingUrl {
+    pub fn from_str(input: &str) -> Result<Self, ParseError> {
+        let mut subber = SubstitutingUrl {
             segments: Vec::new(),
         };
-        result.parse(input);
+        subber.parse(input)?;
 
-        result
+        Ok(subber)
     }
 
-    fn parse(&mut self, remainder: &str) {
+    fn parse(&mut self, remainder: &str) -> Result<(), ParseError> {
         if remainder.is_empty() {
-            return;
+            return Ok(());
         }
 
         if remainder.starts_with(r"\{") {
@@ -36,14 +36,13 @@ impl SubstitutingUrl {
 
                 self.segments
                     .push(SubstitutingSegment::Plain(plain.to_owned()));
-                self.parse(remainder);
-                return; // url_replace_inner(result, to_add, remainder, values);
+                return self.parse(remainder);
             }
 
             self.segments.push(SubstitutingSegment::Plain(
                 remainder[plain_start..].to_owned(),
             ));
-            return;
+            return Ok(());
         }
 
         if remainder.starts_with("{") {
@@ -52,15 +51,17 @@ impl SubstitutingUrl {
                 let rem_start = name_end + "}".len();
 
                 let name = &remainder[name_start..name_end];
+                if name.is_empty() {
+                    return Err(ParseError::EmptyName);
+                }
                 let remainder = &remainder[rem_start..];
 
                 self.segments
                     .push(SubstitutingSegment::Variable(name.to_owned()));
-                self.parse(remainder);
-                return; // url_replace_inner(result, val, remainder, values);
+                return self.parse(remainder);
             }
 
-            return; // Err("Unterminated variable tag");
+            return Err(ParseError::UnterminatedVariableTag);
         }
 
         if let Some(pos) = find_interest(remainder) {
@@ -68,16 +69,18 @@ impl SubstitutingUrl {
 
             self.segments
                 .push(SubstitutingSegment::Plain(plain.to_owned()));
-            self.parse(remainder);
-            return;
+            return self.parse(remainder);
         }
 
         self.segments
             .push(SubstitutingSegment::Plain(remainder.to_owned()));
-        return;
+        return Ok(());
     }
 
-    pub fn sub_by_name(&self, values: &HashMap<String, String>) -> Cow<str> {
+    pub fn sub_by_name(
+        &self,
+        values: &HashMap<String, String>,
+    ) -> Result<Cow<str>, SubstitutionError> {
         let mut result = String::from("");
 
         for segment in &self.segments {
@@ -91,16 +94,34 @@ impl SubstitutingUrl {
                     if let Some(val) = values.get(name) {
                         result
                             .write_str(&val)
-                            .expect(&format!("no value for variable: {}", name));
+                            .expect("unknown error when writing parsed URL");
+                    } else {
+                        return Err(SubstitutionError::NoSuchVariable(name.clone()));
                     }
                 }
             }
         }
 
-        result.into()
+        Ok(result.into())
     }
 
-    pub fn sub_by_index(&self, values: &[String]) -> Cow<str> {
+    pub fn sub_by_index(&self, values: &[String]) -> Result<Cow<str>, SubstitutionError> {
+        let var_count = self
+            .segments
+            .iter()
+            .filter(|x| match x {
+                SubstitutingSegment::Variable(_) => true,
+                _ => false,
+            })
+            .count();
+        let val_count = values.iter().count();
+        if val_count < var_count {
+            return Err(SubstitutionError::WrongNumberVariables {
+                expected: var_count,
+                found: val_count,
+            });
+        }
+
         let mut result = String::from("");
         let mut values = values.iter().rev();
 
@@ -115,13 +136,15 @@ impl SubstitutingUrl {
                     if let Some(val) = values.next() {
                         result
                             .write_str(&val)
-                            .expect(&format!("not enough values for variable: {}", name));
+                            .expect("unknown error when writing parsed URL");
+                    } else {
+                        return Err(SubstitutionError::NoSuchVariable(name.clone()));
                     }
                 }
             }
         }
 
-        result.into()
+        Ok(result.into())
     }
 }
 
@@ -135,33 +158,45 @@ mod url_replace_tests {
 
     #[test]
     fn test_plain() {
-        let subber = SubstitutingUrl::from_str("test/something/blah");
+        let subber = SubstitutingUrl::from_str("test/something/blah").unwrap();
 
-        let result = subber.sub_by_name(&HashMap::new());
+        let result = subber.sub_by_name(&HashMap::new()).unwrap();
         assert_eq!(result, "test/something/blah".to_owned());
     }
 
     #[test]
     fn test_parse() {
-        let subber = SubstitutingUrl::from_str("test/{val}/blah");
+        let subber = SubstitutingUrl::from_str("test/{val}/blah").unwrap();
 
         let mut test_values = HashMap::new();
         test_values.insert("val".to_owned(), "something".to_owned());
 
-        let result = subber.sub_by_name(&test_values);
+        let result = subber.sub_by_name(&test_values).unwrap();
         assert_eq!(result, "test/something/blah".to_owned());
     }
 
     #[test]
     fn test_multi_parse() {
-        let subber = SubstitutingUrl::from_str("{v1}/{v2}{v3}");
+        let subber = SubstitutingUrl::from_str("{v1}/{v2}{v3}").unwrap();
 
         let mut test_values = HashMap::new();
         test_values.insert("v1".to_owned(), "la".to_owned());
         test_values.insert("v2".to_owned(), "de".to_owned());
         test_values.insert("v3".to_owned(), "dah".to_owned());
 
-        let result = subber.sub_by_name(&test_values);
+        let result = subber.sub_by_name(&test_values).unwrap();
         assert_eq!(result, "la/dedah".to_owned());
     }
+}
+
+#[derive(Debug)]
+pub enum ParseError {
+    EmptyName,
+    UnterminatedVariableTag,
+}
+
+#[derive(Debug)]
+pub enum SubstitutionError {
+    NoSuchVariable(String),
+    WrongNumberVariables { expected: usize, found: usize },
 }
